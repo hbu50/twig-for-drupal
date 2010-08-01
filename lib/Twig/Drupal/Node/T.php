55370d81adf0391653d2f0bbbdf8643b6ddeba08
Rene Bakx <rene@71media.net>
2010-08-01 15:47:24 +0200

    working version of the drupal t() method in twig-lingo

--- lib/Twig/Drupal/Node/T.php
+++ lib/Twig/Drupal/Node/T.php
@@ -1,16 +1,18 @@
 <?php
+/* The actual twig->node to drupal T() function mapper  */
+
 Class Twig_Drupal_Node_T extends Twig_Node {
 
-    public function __construct($value, $lineno) {
-        parent::__construct(array(), array('data' => $value), $lineno);
+    public function __construct($expressions) {
+        parent::__construct(array(), $expressions, $lineno);
     }
-    
 
     public function compile($compiler) {
-        $compiler->addDebugInfo($this)
-                ->write("echo t(")
-                ->string($this['data'])
-                ->write(");");
+        if(isset($this['lang'])) {
+            $compiler->write("echo t('".$this['string']."',array(),'".$this['lang']."');");
+        } else {
+            $compiler->write("echo t('".$this['string']."');");
+        }
     }
 }
 ?>
--- lib/Twig/Drupal/TokenParser/T.php
+++ lib/Twig/Drupal/TokenParser/T.php
@@ -1,16 +1,47 @@
 <?php
-/* 
- * Maps the translate tag to the drupal t() function
+/* Maps the T tag to the drupal t() function
+ * 
+ * usage :
+ * {%t "foo" %}  translage foo
+ * or
+ * {%t string="foo" lang="bar" %} translate foo in language bar
+ *
 */
 
 class Twig_Drupal_TokenParser_T extends Twig_TokenParser {
 
-
     public function parse(Twig_Token $token) {
+
+        $lineno = $token->getLine();
         $stream = $this->parser->getStream();
-        $value = trim($stream->expect(Twig_Token::STRING_TYPE)->getValue());
+        $expressions = array();
+
+        if (!$stream->test(Twig_Token::BLOCK_END_TYPE)) {
+            if($stream->test(Twig_Token::STRING_TYPE)) {
+                $expressions[] = array("lineno" => $lineno,"string" => $stream->getCurrent()->getValue());
+                $stream->next();
+            }elseif($stream->test(Twig_Token::NAME_TYPE)) {
+                $expressions[] = $this->parseComplex($stream);
+            }
+        }
         $stream->expect(Twig_Token::BLOCK_END_TYPE);
-        return new Twig_Drupal_Node_T($value,$token->getLine());
+        return new Twig_Drupal_Node_T($expressions[0]);
+    }
+
+    private function parseComplex(&$stream) {
+        $parameters = array();
+        while (!$stream->test(Twig_Token::BLOCK_END_TYPE)) {
+            // parameters should always be name->operator->string
+            if ($stream->getCurrent()->getType() == Twig_Token::NAME_TYPE) {
+                $parameters["lineno"] = $this->parser->getCurrentToken()->getLine();
+                $name = strtolower($stream->getCurrent()->getValue());
+                $stream->next();
+                $stream->expect(Twig_Token::OPERATOR_TYPE);
+                $parameters[$name] = $stream->getCurrent()->getValue();
+            }
+            $stream->next();
+        }
+        return $parameters;
     }
 
     public function getTag() {
